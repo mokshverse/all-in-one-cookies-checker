984cff881a78217d90ce3ac61d40662788713c71
Moksh <226147721+mokshverse@users.noreply.github.com>
2026-05-23 03:27:10 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN   = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-MAX_THREADS = int(os.getenv("MAX_THREADS", "30"))
-TIMEOUT     = int(os.getenv("TIMEOUT", "18"))
